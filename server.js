821590b3bdf48812f480fd389bbec7763b3b749f
tiea9999 <155359022+tiea9999@users.noreply.github.com>
2025-12-11 12:24:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-// สร้างช่อง auto ch9–ch100
+// สร้างช่อง auto ch1–ch100
 const channels = {};
 for (let i = 9; i <= 100; i++) {
   channels[`ch${i}`] = `http://119.59.118.159/live/ch${i}/xxccxgd134/`;
